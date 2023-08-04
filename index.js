0efbc54d60b327d3c1e7a4b266ccf247a2f97dc4
V.MOHANRAJ <95488698+mohan300404@users.noreply.github.com>
2023-08-04 18:38:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ playerName2 = playerName2.toUpperCase();
         var randomNumber1 = Math.floor((Math.random() * 6) + 1);
         // console.log(randomNumber);
         var randomImage =  "dice" + randomNumber1 +".png";
-        var randomImageSource = "images/" + randomImage ;
+        var randomImageSource =randomImage ;
         var image1 = document.querySelectorAll("img")[0];
         image1.setAttribute("src" , randomImageSource);
     
         var randomNumber2 = Math.floor((Math.random() *6) + 1);
     
         var randomImage2="dice" + randomNumber2 + ".png";
-        var randomImageSource2 = "images/" + randomImage2;
+        var randomImageSource2 = randomImage2;
         var image2 = document.querySelectorAll("img")[1];
         image2.setAttribute("src" , randomImageSource2);
 
